fb05fd2d9ede0d2a63558d97bec69f622192b352
Tevezx <teveztwitch@gmail.com>
2026-06-23 22:31:59 -0300

    adding method updated to anime and producer

--- anime-service/src/main/java/academy/devdojo/mapper/ProducerMapper.java
+++ anime-service/src/main/java/academy/devdojo/mapper/ProducerMapper.java
@@ -2,12 +2,14 @@ package academy.devdojo.mapper;
 
 import academy.devdojo.domain.Producer;
 import academy.devdojo.request.ProducerPostRequest;
+import academy.devdojo.request.ProducerPutRequest;
 import academy.devdojo.response.ProducerGetResponse;
 import academy.devdojo.response.ProducerPostResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
@@ -20,7 +22,10 @@ public interface ProducerMapper {
     Producer toProducer(ProducerPostRequest postRequest);
     ProducerPostResponse toProducerPostResponse(Producer producer);
 
+    Producer toProducerUpdated(ProducerPutRequest producerPutRequest, LocalDateTime createdAt);
+
     // primeiro é quem desejamos retornar
     ProducerGetResponse toProducerGetResponse(Producer producer);
     List<ProducerGetResponse> toProducerGetResponseList(List<Producer> producers);
+
 }
